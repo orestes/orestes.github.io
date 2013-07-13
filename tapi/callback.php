e2f5eb284a9f77d944bfc7ee4c4a75ce2e205e90
orestes <orestes.ca@gmail.com>
2013-07-13 22:59:50 +0200

    Initial commit. Imported from orestescarracedo.com

---
+++ tapi/callback.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+/* Request access tokens from twitter */
+$access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
+
+/* Save the access tokens. Normally these would be saved in a database for future use. */
+$_SESSION['access_token'] = $access_token;
